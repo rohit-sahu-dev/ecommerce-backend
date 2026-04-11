cf4b8edc95f3556db127725ddf3c1cd0b2235019
Rohit Sahu <sahu.rohit2101@gmail.com>
2026-04-12 03:11:02 +0530

    Set authentication in SecurityContext using JWT for secured endpoints

--- src/main/java/com/ecommerce/backend/security/JwtFilter.java
+++ src/main/java/com/ecommerce/backend/security/JwtFilter.java
@@ -2,9 +2,13 @@ package com.ecommerce.backend.security;
 
 import jakarta.servlet.*;
 import jakarta.servlet.http.*;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 
 import java.io.IOException;
+import java.util.Collections;
 
 @Component
 public class JwtFilter extends GenericFilter {
@@ -23,6 +27,16 @@ public class JwtFilter extends GenericFilter {
 
             try {
                 String email = JwtUtil.extractEmail(token);
+
+                UsernamePasswordAuthenticationToken auth =
+                        new UsernamePasswordAuthenticationToken(
+                                email,
+                                null,
+                                Collections.singletonList(new SimpleGrantedAuthority("USER"))
+                        );
+
+                SecurityContextHolder.getContext().setAuthentication(auth);
+
                 System.out.println("Authenticated User: " + email);
             } catch (Exception e) {
                 System.out.println("Invalid JWT Token");
